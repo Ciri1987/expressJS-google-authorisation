8b5c6de50e930c01bad9c32f358b9600374fdbed
Anna Strzała <strzala.ania@gmail.com>
2019-03-11 17:32:13 +0100

    Add all files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '337527952883-3gsftg81ds095hf3lrtudcr70evo6k9f.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '3yt51PCkNdqkxQ5jTRSQrQp0',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
